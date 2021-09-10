e137f50511381c42ddf9a82ac5cbeb310b250f0c
DennisWanjeri <shikandadennis07@gmail.com>
2021-09-10 08:33:10 +0000

    web framework

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 """
-starts a flask web application
-The application listens on localhost port 5000
+starts a Flask web application
 """
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """
+    Displays 'Hello HBNB!'
+    """
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_display():
+    """
+    displays "HBNB"
+    """
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
