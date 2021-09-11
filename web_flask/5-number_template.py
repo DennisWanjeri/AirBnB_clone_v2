8ac0506282c76d2a3495c5293f26b37c9a7ce843
DennisWanjeri <shikandadennis07@gmail.com>
2021-09-11 11:23:02 +0000

    render template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,6 +3,7 @@
 starts a Flask web application
 """
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -38,6 +39,19 @@ def python(text="is cool"):
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    Displays 'n is a number' only if n is an integer.
+    """
+    return "{} is a number".format(n)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """
+    Displays an HTML page only if <n> is an integer.
+    """
+    return render_template("5-number.html", n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
