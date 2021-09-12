773dac6bf68c7485be65a69b3e28a30a4f16723e
DennisWanjeri <shikandadennis07@gmail.com>
2021-09-12 10:02:25 +0000

    number odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -6,7 +6,8 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
@@ -52,6 +53,14 @@ def number_template(n):
     Displays an HTML page only if <n> is an integer.
     """
     return render_template("5-number.html", n=n)
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    Displays an HTML page only if <n> is an integer .
+    Displays whether <n> is even or odd .
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
